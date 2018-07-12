caa9827144e1861208123698dc20f699cfbc948f
lpenzey <lucaspenzeymoog@gmail.com>
2018-07-12 10:38:43 -0500

    restarted from scratch

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,26 +1,10 @@
-class Game
-	attr_reader :score, :frame_count, :frame_roll, :spare
+class Bowling
 
-	def initialize
-		@score = 0
-		@frame_count = 0
-		@frame_roll = 1
-		@spare = false
-	end
+def score(rolls)
+	rolls.sum 
 
-	def roll(pins = rand(10))
-		if @frame_roll == 1
-			@score
-		end
-
-		if spare
-			@score += 2 * pins
-		else
-			@score += pins
-		end
-	end
-
-	def spare
-		roll
-	end
 end
+
+
+
+end 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,31 +1,18 @@
-require "bowling"
+require 'bowling'
 
-RSpec.describe Game, "when bowling" do
-	let(:bowling) { Game.new }
+RSpec.describe Bowling do 
 	describe "#score" do 
-		it "all gutters result to a score of 0" do
-			20.times do 
-				bowling.roll(0)
-			end
-			expect(bowling.score).to eq 0
-
+		it "returns 0 if all rolls are 0" do
+			rolls = Array.new(20, 0)
+			game = Bowling.new
+			expect(game.score(rolls)).to eq 0
+		end
+		it "returns 20 if all rolls are 1" do
+			rolls = Array.new(20, 1)
+			game = Bowling.new
+			expect(game.score(rolls)).to eq 20
 		end
 
-		it "20 rolls of one equals score of 20" do
-			20.times do 
-				bowling.roll(1)
-			end
-			expect(bowling.score).to eq 20
-		end
+	end
 
-		it "one spare equals 10 points in a frame" do
-			4.times do 
-				bowling.roll(5)
-			end
-			expect(bowling.score).to eq 25 
-		end
-
-		it "one strike..."
-		end
-		
-end
+end 
