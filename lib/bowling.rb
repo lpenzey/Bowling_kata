a96f1fdfb52ecc453a30ff37c56b5ab2e804e3f1
lpenzey <lucaspenzeymoog@gmail.com>
2018-07-13 16:53:44 -0500

    added spare logic

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,10 +1,21 @@
+require 'pry'
 class Bowling
 
-def score(rolls)
-	rolls.sum 
-
-end
-
-
+	def score(rolls)
+		@sum = 0
+		first_roll = 0
+		second_roll = first_roll + 1
+		while rolls[first_roll] do
+			break if rolls[second_roll + 1].nil?
+			if rolls[first_roll..second_roll].sum == 10
+				@sum += rolls[second_roll + 1]
+			end
+			first_roll += 1
+			second_roll += 1
+		end
+		@sum += rolls.sum
+	end
 
 end 
+
+Bowling.new.score([6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6])
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,17 +1,39 @@
 require 'bowling'
 
 RSpec.describe Bowling do 
+	let(:game) { Bowling.new }
+
 	describe "#score" do 
 		it "returns 0 if all rolls are 0" do
 			rolls = Array.new(20, 0)
-			game = Bowling.new
 			expect(game.score(rolls)).to eq 0
 		end
+		
 		it "returns 20 if all rolls are 1" do
 			rolls = Array.new(20, 1)
-			game = Bowling.new
 			expect(game.score(rolls)).to eq 20
 		end
+
+		it "returns 26 when bowler rolls 5, 5, 7, 2" do
+			rolls = [5, 5, 7, 2]
+			expect(game.score(rolls)).to eq 26
+		end
+
+		it "returns 26 when bowler rolls 5, 5, 7, 2" do
+			rolls = [5, 5, 3, 2, 6, 4, 7, 1]
+			expect(game.score(rolls)).to eq 43
+		end
+
+		it "returns 160 when bowler rolls 6, 4 spares the entire game" do
+			rolls = [6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6]
+			expect(game.score(rolls)).to eq 160
+		end
+
+		xit "returns 300 is all rolls are strikes" do
+			rolls = [10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10]
+			expect(game.score(rolls)).to eq 300
+		end
+
 
 	end
 
