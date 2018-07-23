8eabca20faa97af0222f778c9d6db2a756545520
lpenzey <lucaspenzeymoog@gmail.com>
2018-07-23 11:00:03 -0500

    algorithm counts spares and single strikes

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -4,16 +4,21 @@ class Bowling
 	def score(rolls)
 		@sum = 0
 		first_roll = 0
-		second_roll = first_roll + 1
-		while rolls[first_roll] do
-			break if rolls[second_roll + 1].nil?
-			if rolls[first_roll..second_roll].sum == 10
-				@sum += rolls[second_roll + 1]
+		second_roll = 1
+		#binding.pry;
+		while second_roll < rolls.length 
+			roll_sum = rolls[first_roll] + rolls[second_roll]
+			if rolls[first_roll] == 10
+				@sum += 10 + rolls[first_roll + 2] + rolls[second_roll + 2]
+			elsif roll_sum == 10
+				@sum += rolls[second_roll + 1] + roll_sum
+			else 
+				@sum += roll_sum
 			end
-			first_roll += 1
-			second_roll += 1
+			first_roll += 2
+			second_roll += 2
 		end
-		@sum += rolls.sum
+		@sum
 	end
 
 end 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Bowling do
 			expect(game.score(rolls)).to eq 26
 		end
 
-		it "returns 26 when bowler rolls 5, 5, 7, 2" do
+		it "returns 43 when bowler rolls 5, 5, 3, 2, 6, 4, 7, 1" do
 			rolls = [5, 5, 3, 2, 6, 4, 7, 1]
 			expect(game.score(rolls)).to eq 43
 		end
@@ -27,6 +27,16 @@ RSpec.describe Bowling do
 		it "returns 160 when bowler rolls 6, 4 spares the entire game" do
 			rolls = [6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6]
 			expect(game.score(rolls)).to eq 160
+		end
+
+		it "returns 31 when bowler rolls 10, 0, 5, 3, 2, 3" do
+			rolls = [10, 0, 5, 3, 2, 3] 
+			expect(game.score(rolls)).to eq 31
+		end
+
+		it "returns 51 when bowler rolls 10, 0, 10, 0, 5, 3" do
+			rolls = [10, 0, 10, 0, 5, 3] 
+			expect(game.score(rolls)).to eq 51
 		end
 
 		xit "returns 300 is all rolls are strikes" do
